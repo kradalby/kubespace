4ba9cbfac8f901f7105b72c7f0b68a07108b0319
Kristoffer Dalby <kradalby@kradalby.no>
2018-09-04 10:00:00 +0200

    makes this thing a bit reasonable

---
+++ setup.py
@@ -0,0 +1,23 @@
+__author__ = "Kristoffer Dalby <kradalby@kradalby.no>"
+__version__ = "0.0.1"
+
+from setuptools import setup
+import sys
+
+
+setup(
+    name="libsandwich",
+    version=__version__,
+    description="Tool (kubectl wrapper) to create namespaces and service accounts that can safely be handed to CI or users.",
+    author=__author__,
+    author_email="kradalby@kradalby.no",
+    url="https://github.com/kradalby/kubespace",
+    license="MIT",
+    # Ansible will also make use of a system copy of python-six and
+    # python-selectors2 if installed but use a Bundled copy if it's not.
+    # install_requires=install_requirements,
+    scripts=["kubespace"],
+    data_files=[],
+    # Installing as zip files would break due to references to __file__
+    zip_safe=False,
+)
